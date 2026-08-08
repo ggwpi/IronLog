5432492010d9b638761e49babf68fc9fdc840efa
ggwpi <ggwphomo@gmail.com>
2026-08-08 18:33:07 +0300

    Refresh PWA cache for Big Screen v6

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ironlog-v4-7';
-const CORE=['/','/index.html','/src/styles.css','/src/data.js','/src/storage.js','/src/app.js','/src/bigscreen-v2.js','/src/bigscreen-v4.js','/src/bigscreen-v5.js','/manifest.webmanifest'];
+const CACHE='ironlog-v4-8';
+const CORE=['/','/index.html','/src/styles.css','/src/data.js','/src/storage.js','/src/app.js','/src/bigscreen-v2.js','/src/bigscreen-v4.js','/src/bigscreen-v5.js','/src/bigscreen-v6.js','/manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
