6dc0e7a9528239eace75faf8e4908188fc558fbf
ggwpi <ggwphomo@gmail.com>
2026-08-08 16:02:01 +0300

    Bump IronLog offline cache after workout fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ironlog-v3-1';
+const CACHE='ironlog-v3-2';
 const CORE=['/','/index.html','/src/styles.css','/src/data.js','/src/storage.js','/src/app.js','/manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
