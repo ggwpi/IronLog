43103fc101b656926a925d5c25d77a2b50a7b8ea
ggwpi <ggwphomo@gmail.com>
2026-08-09 20:56:30 +0300

    Normalize anatomy PNG metadata

--- src/assets/anatomy-assets.js
+++ src/assets/anatomy-assets.js
@@ -1,10 +1,10 @@
 export const ANATOMY_ASSETS = Object.freeze({
   'push-a': Object.freeze({ src: '/assets/anatomy/push-a.png', width: 309, height: 540 }),
-  'legs-a': Object.freeze({ src: '/assets/anatomy/legs-a.png', width: 559, height: 510 }),
+  'legs-a': Object.freeze({ src: '/assets/anatomy/legs-a.png', width: 395, height: 360 }),
   'pull-a': Object.freeze({ src: '/assets/anatomy/pull-a.png', width: 268, height: 540 }),
   'push-b': Object.freeze({ src: '/assets/anatomy/push-b.png', width: 309, height: 540 }),
-  'legs-b': Object.freeze({ src: '/assets/anatomy/legs-b.png', width: 559, height: 510 }),
-  arms: Object.freeze({ src: '/assets/anatomy/arms.png', width: 559, height: 510 }),
+  'legs-b': Object.freeze({ src: '/assets/anatomy/legs-b.png', width: 395, height: 360 }),
+  arms: Object.freeze({ src: '/assets/anatomy/arms.png', width: 309, height: 540 }),
 });
 
 export function anatomyAsset(id) {
