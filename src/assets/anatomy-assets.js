cf661f7f25ebfca40dd9ddb7ef4d780190330543
ggwpi <ggwphomo@gmail.com>
2026-08-09 22:58:51 +0300

    Version canonical anatomy asset URLs

--- src/assets/anatomy-assets.js
+++ src/assets/anatomy-assets.js
@@ -1,10 +1,13 @@
+const ASSET_VERSION = '20';
+const src = (name) => `/assets/anatomy/${name}.png?v=${ASSET_VERSION}`;
+
 export const ANATOMY_ASSETS = Object.freeze({
-  'push-a': Object.freeze({ src: '/assets/anatomy/push-a.png', width: 309, height: 540 }),
-  'legs-a': Object.freeze({ src: '/assets/anatomy/legs-a.png', width: 395, height: 360 }),
-  'pull-a': Object.freeze({ src: '/assets/anatomy/pull-a.png', width: 268, height: 540 }),
-  'push-b': Object.freeze({ src: '/assets/anatomy/push-b.png', width: 309, height: 540 }),
-  'legs-b': Object.freeze({ src: '/assets/anatomy/legs-b.png', width: 395, height: 360 }),
-  arms: Object.freeze({ src: '/assets/anatomy/arms.png', width: 309, height: 540 }),
+  'push-a': Object.freeze({ src: src('push-a'), width: 309, height: 540 }),
+  'legs-a': Object.freeze({ src: src('legs-a'), width: 395, height: 360 }),
+  'pull-a': Object.freeze({ src: src('pull-a'), width: 268, height: 540 }),
+  'push-b': Object.freeze({ src: src('push-b'), width: 309, height: 540 }),
+  'legs-b': Object.freeze({ src: src('legs-b'), width: 395, height: 360 }),
+  arms: Object.freeze({ src: src('arms'), width: 309, height: 540 }),
 });
 
 export function anatomyAsset(id) {
