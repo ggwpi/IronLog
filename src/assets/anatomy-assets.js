e355e17e3f68ed58baae66c2117430a609a1697c
ggwpi <ggwphomo@gmail.com>
2026-08-09 16:31:15 +0300

    Switch anatomy registry to PNG masters

--- src/assets/anatomy-assets.js
+++ src/assets/anatomy-assets.js
@@ -1,10 +1,10 @@
 export const ANATOMY_ASSETS = Object.freeze({
-  'push-a': Object.freeze({ src: '/assets/anatomy/push-a.webp', width: 309, height: 540 }),
-  'legs-a': Object.freeze({ src: '/assets/anatomy/legs-a.webp', width: 559, height: 510 }),
-  'pull-a': Object.freeze({ src: '/assets/anatomy/pull-a.webp', width: 268, height: 540 }),
-  'push-b': Object.freeze({ src: '/assets/anatomy/push-b.webp', width: 309, height: 540 }),
-  'legs-b': Object.freeze({ src: '/assets/anatomy/legs-b.webp', width: 559, height: 510 }),
-  arms: Object.freeze({ src: '/assets/anatomy/arms.webp', width: 559, height: 510 }),
+  'push-a': Object.freeze({ src: '/assets/anatomy/push-a.png', width: 309, height: 540 }),
+  'legs-a': Object.freeze({ src: '/assets/anatomy/legs-a.png', width: 559, height: 510 }),
+  'pull-a': Object.freeze({ src: '/assets/anatomy/pull-a.png', width: 268, height: 540 }),
+  'push-b': Object.freeze({ src: '/assets/anatomy/push-b.png', width: 309, height: 540 }),
+  'legs-b': Object.freeze({ src: '/assets/anatomy/legs-b.png', width: 559, height: 510 }),
+  arms: Object.freeze({ src: '/assets/anatomy/arms.png', width: 559, height: 510 }),
 });
 
 export function anatomyAsset(id) {
