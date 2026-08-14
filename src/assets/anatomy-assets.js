8408d76fc8eded43a5b0ab42c05d90957b8d2293
ggwpi <ggwphomo@gmail.com>
2026-08-14 13:32:31 +0300

    Switch workout anatomy to Retina SVG assets

--- src/assets/anatomy-assets.js
+++ src/assets/anatomy-assets.js
@@ -1,13 +1,13 @@
-const ASSET_VERSION = '21';
-const src = (name) => `/assets/anatomy/${name}.png?v=${ASSET_VERSION}`;
+const ASSET_VERSION = '22';
+const src = (name) => `/assets/anatomy/${name}.svg?v=${ASSET_VERSION}`;
 
 export const ANATOMY_ASSETS = Object.freeze({
-  'push-a': Object.freeze({ src: src('push-a'), width: 105, height: 260 }),
-  'legs-a': Object.freeze({ src: src('legs-a'), width: 220, height: 260 }),
-  'pull-a': Object.freeze({ src: src('pull-a'), width: 109, height: 260 }),
-  'push-b': Object.freeze({ src: src('push-b'), width: 100, height: 260 }),
-  'legs-b': Object.freeze({ src: src('legs-b'), width: 206, height: 260 }),
-  arms: Object.freeze({ src: src('arms'), width: 199, height: 260 }),
+  'push-a': Object.freeze({ src: src('push-a'), width: 720, height: 980 }),
+  'legs-a': Object.freeze({ src: src('legs-a'), width: 720, height: 980 }),
+  'pull-a': Object.freeze({ src: src('pull-a'), width: 720, height: 980 }),
+  'push-b': Object.freeze({ src: src('push-b'), width: 720, height: 980 }),
+  'legs-b': Object.freeze({ src: src('legs-b'), width: 720, height: 980 }),
+  arms: Object.freeze({ src: src('arms'), width: 720, height: 980 }),
 });
 
 export function anatomyAsset(id) {
